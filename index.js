2bbffc92d6ee08a6a65fd52aa2fda57ef5920ae5
Nikolas Mazur <kolemazur@gmail.com>
2022-04-30 09:27:07 -0600

    Add start prompt

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+const start = async () => {
+    const answer = await inquirer.prompt([
+      {
+        type: "list",
+        name: "startUp",
+        message: "What would you like to do?",
+        choices: ["Add a team", "Close"],
+      },
+    ]);
+};
