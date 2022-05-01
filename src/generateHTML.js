40a08cfcf18496eb64853c8b34a276ac69ffcdb6
Nikolas Mazur <kolemazur@gmail.com>
2022-04-30 23:54:13 -0600

    Add card generation functions

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,6 +1,33 @@
 const fs = require("fs");
 const Employee = require("../lib/Employee");
 
+const generate = (employeesArr) => {
+    // Creates managers card information
+    function getManagers(employee) {
+        for (i = 0; i < employee.length; i++) {
+            if (employee[i].role === "Manager") {
+                managerCardsContent.push(employee[i].managerCard);
+            }
+        }
+    }
+    // Creates engineers card information
+    function getEmgineers(employee) {
+        for (i = 0; i < employee.length; i++) {
+            if (employee[i].role === "Engineer") {
+                engineerCardsContent.push(employee[i].engineerCard);
+            }
+        }
+    }
+    // Creates inters card information
+    function getInters(employee) {
+        for (i = 0; i < employee.length; i++) {
+            if (employee[i].role === "Intern") {
+                internCardsContent.push(employee[i].internCard);
+            }
+        }
+    }
+}
+
 // Funtion to write file and path
 const generatePage = (employeesArr) => {
     fs.writeFile('./dist/MyTeam.html', generate(employeesArr), function(err, result) {
