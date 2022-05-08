98c9c9a80248a32b890157f789cab9ea56c91604
Nikolas Mazur <kolemazur@gmail.com>
2022-05-08 15:01:36 -0600

    Add additional HTML/Bootstrap elements to generation

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -46,16 +46,37 @@ const generate = (employeesArr) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="./assets/css/style.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <title>My Team</title>
     </head>
 
     <body>
-        <header>
-            <h1>My Team</h1>
+        <header class="jumbotron">
+            <div class="container">
+                <div class="row align-self-center">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
         </header>
 
         <main>
-
+            <section class='container'>
+                <div class='row'>
+                    ${managerCardsContent.join(" ")}
+                </div>
+            </section>
+            <hr class='mt-1 mb-4'/>
+            <section class='container'>
+                <div class='row'>
+                    ${engineerCardsContent.join(" ")}
+                </div>
+            </section>
+            <hr class='mt-1 mb-4'/>
+            <section class='container'>
+                <div class='row'>
+                    ${internCardsContent.join(" ")}
+                </div>
+            </section>
         </main>
     </body>
     </html>`
